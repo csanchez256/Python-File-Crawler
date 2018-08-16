008fd5ca513d2b91b56cd3134f97eb0cba454827
chris <css7c3@gmail.com>
2018-08-15 21:25:49 -0600

    added input options

--- fileCrawl.py
+++ fileCrawl.py
@@ -1,14 +1,17 @@
-# Run this python program in the terminal. Change the rootDir to 
-# the folder that you want to start traversing
+# Run this python program in the terminal. 
+# Type in the terminal the folder that you want to start traversing
 # Chris Sanchez 8/15/18
 
 import os, shutil
-import glob
 
 # define the directory you want to crawl
 workingDirectory = os.getcwd()
 
-target_file = input('Enter name of file to copy: ')
+target_file = input('Enter name of directory to copy: ')
+
+file_input = input('Enter the type of file don\'t want copied: ')
+
+file_extension = '.' + file_input
 
 rootDir = workingDirectory + '/' + target_file
 
@@ -18,7 +21,7 @@ destination = workingDirectory + '/' + target_file + '_Copy'
 # Clone the entire directory
 shutil.copytree(rootDir,destination,symlinks=False,ignore=None);
 
-def removeFiles(destination):
+def removeFiles(destination, file_input):
 	# for all folders, subfolders etc... walk through
 	for dirName, subdirList, fileList in os.walk(destination):
 		print('Found directory: %s' % dirName)
@@ -26,8 +29,11 @@ def removeFiles(destination):
     	# If the file ends in .wmv, then delete it
 		for file_name in fileList:
 			print('\t%s' % file_name)
-			if (file_name.endswith(".wmv")):
+			if (file_name.endswith('.' + file_input)):
 				os.remove(os.path.join(dirName, file_name))
 
 
-removeFiles(destination)
+removeFiles(destination, file_input)
+
+input("Press Enter to quit")
+
